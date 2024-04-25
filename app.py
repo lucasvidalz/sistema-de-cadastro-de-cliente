c0f5f0bd356e7887e2b6e2c4280aa4e415c7a637
lucasvidal <lucasvidalk9@gmail.com>
2024-04-25 02:43:48 -0300

    first commit - project 1.0

---
+++ app.py
@@ -0,0 +1,145 @@
+import customtkinter as ctk
+from tkinter import *
+from tkinter import messagebox
+import openpyxl
+import pathlib
+from openpyxl import Workbook
+
+# Setando a aparencia padrão do sistema
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.layout_config()
+        self.appearence()
+        self.todo_sistema()
+        self.create_widgets()  # Chama a função para criar os widgets
+
+    def layout_config(self):
+        self.title("Sistema de Gestão de Clientes")
+        self.geometry("700x500")
+
+    def appearence(self):
+        self.lb_apm = ctk.CTkLabel(self, text="Tema", bg_color="transparent", text_color=["#000", "#fff"])
+        self.lb_apm.place(x=50, y=430)
+        self.opt_apm = ctk.CTkOptionMenu(self, values=["System", "Dark", "Light"], command=self.change_apm)
+        self.opt_apm.place(x=50, y=460)
+
+    def create_widgets(self):  
+        # texts variables
+        self.name_value = StringVar()
+        self.contact_value = StringVar()
+        self.age_value = StringVar()
+        self.address_value = StringVar()
+
+        # entrys
+        self.name_entry = ctk.CTkEntry(self, width=350, textvariable=self.name_value, font=("Century Gothic bold", 16), fg_color="transparent")
+        self.contact_entry = ctk.CTkEntry(self, width=200, textvariable=self.contact_value, font=("Century Gothic bold", 16), fg_color="transparent")
+        self.age_entry = ctk.CTkEntry(self, width=150, textvariable=self.age_value, font=("Century Gothic bold", 16), fg_color="transparent")
+        self.address_entry = ctk.CTkEntry(self, width=200, textvariable=self.address_value, font=("Century Gothic bold", 16), fg_color="transparent")
+
+        # combobox
+        self.gender_combobox = ctk.CTkComboBox(self, values=["Masculino", "Feminino"], font=("Century Gothic bold", 14))
+        self.gender_combobox.set("Masculino")
+
+        # entrada de observações
+        self.obs_entry = ctk.CTkTextbox(self, width=470, height=150, font=("arial", 18), border_color="#aaa", border_width=2, fg_color="transparent")
+
+        # labels
+        self.lb_name = ctk.CTkLabel(self, text="Nome Completo:", font=("Century Gothic bold", 16), text_color=["#000", "#fff"])
+        self.lb_contact = ctk.CTkLabel(self, text="Contato:", font=("Century Gothic bold", 16), text_color=["#000", "#fff"])
+        self.lb_age = ctk.CTkLabel(self, text="Idade:", font=("Century Gothic bold", 16), text_color=["#000", "#fff"])
+        self.lb_gender = ctk.CTkLabel(self, text="Gênero:", font=("Century Gothic bold", 16), text_color=["#000", "#fff"])
+        self.lb_address = ctk.CTkLabel(self, text="Endereço:", font=("Century Gothic bold", 16), text_color=["#000", "#fff"])
+        self.lb_obs = ctk.CTkLabel(self, text="Observações:", font=("Century Gothic bold", 16), text_color=["#000", "#fff"])
+
+        # posicionando os elementos na janela
+        self.lb_name.place(x=50, y=120)
+        self.name_entry.place(x=50, y=150)
+
+        self.lb_contact.place(x=450, y=120)
+        self.contact_entry.place(x=450, y=150)
+
+        self.lb_age.place(x=300, y=190)
+        self.age_entry.place(x=300, y=220)
+
+        self.lb_gender.place(x=500, y=190)
+        self.gender_combobox.place(x=500, y=220)
+
+        self.lb_address.place(x=50, y=190)
+        self.address_entry.place(x=50, y=220)
+
+        self.lb_obs.place(x=50, y=260)
+        self.obs_entry.place(x=180, y=260)
+
+        # Botões
+        btn_submit = ctk.CTkButton(self, text="Salvar dados".upper(), command=self.submit, fg_color="#151", hover_color="#131")
+        btn_submit.place(x=300, y=420)
+        
+        btn_clear = ctk.CTkButton(self, text="Limpar Campos".upper(), command=self.clear, fg_color="#555", hover_color="#333")
+        btn_clear.place(x=500, y=420)
+
+    def todo_sistema(self):
+        frame = ctk.CTkFrame(self, width=700, height=50, corner_radius=0, bg_color="teal", fg_color="teal")
+        frame.place(x=0, y=10)
+        title = ctk.CTkLabel(frame, text="Sistema de Gestão de Clientes", font=("Century Gothic bold", 24), text_color="#fff")
+        title.place(x=190, y=10)
+
+        span = ctk.CTkLabel(self, text="Por favor, preencha todos os campos do formulário!", font=("Century Gothic bold", 16), text_color=["#000", "#fff"])
+        span.place(x=50, y=70)
+
+    def submit(self):
+        ficheiro = pathlib.Path("Clientes.xlsx")
+
+        if ficheiro.exists():
+            pass
+        else:
+            ficheiro = Workbook()
+            folha = ficheiro.active
+            folha['A1'] = "Nome Completo"
+            folha['B1'] = "Contato"
+            folha['C1'] = "Idade"
+            folha['D1'] = "Gênero"
+            folha['E1'] = "Endereço"
+            folha['F1'] = "Observações"
+
+            ficheiro.save("Clientes.xlsx")
+
+        name = self.name_value.get()
+        contact = self.contact_value.get()
+        age = self.age_value.get()
+        gender = self.gender_combobox.get()
+        address = self.address_value.get()
+        obs = self.obs_entry.get(0.0, END)
+
+        if name == "" or contact == "" or age == "" or address == "":
+            messagebox.showerror("Sistema", "ERRO!\nPor favor, preencha todos os campos!")
+        else:
+            ficheiro = openpyxl.load_workbook('Clientes.xlsx')
+            folha = ficheiro.active
+            folha.cell(column=1, row=folha.max_row+1, value=name)
+            folha.cell(column=2, row=folha.max_row, value=contact)
+            folha.cell(column=3, row=folha.max_row, value=age)
+            folha.cell(column=4, row=folha.max_row, value=gender)
+            folha.cell(column=5, row=folha.max_row, value=address)
+            folha.cell(column=6, row=folha.max_row, value=obs)
+            
+
+        ficheiro.save(r"Clientes.xlsx")
+        messagebox.showinfo("Sistema", "Dados salvos com sucesso!")
+
+    def clear(self):
+        self.name_value.set("")
+        self.contact_value.set("")
+        self.age_value.set("")
+        self.address_value.set("")
+        self.obs_entry.delete(0.0, END)
+
+    def change_apm(self, nova_aparencia):
+        ctk.set_appearance_mode(nova_aparencia)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
